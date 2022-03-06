b044456739abf6d3b31ac61dec77971de001a713
shyamalanadkat <sanadkat@grubhub.com>
2022-03-06 14:57:44 -0500

    Update model.py

--- scripts/model.py
+++ scripts/model.py
@@ -3,9 +3,14 @@ import pandas as pd
 
 
 def _get_emotion_scores(clf, reviews):
-    return clf(
-        reviews,
-    )
+    review_list = reviews.split("</review>")
+    scores = []
+    for review in review_list[:-1]:
+        cleaned_text = review.replace("<review>", "").replace("[", "").replace("]", "")
+        # print(len(cleaned_text.split()))
+        score = clf(cleaned_text, truncation=True, max_length=512)
+        scores.append(score)
+    return scores
 
 
 if __name__ == "__main__":
@@ -21,15 +26,16 @@ if __name__ == "__main__":
         return_all_scores=True,
     )
 
-    rows_to_keep = [0, 3]
-    reviews_data = pd.read_csv(
-        "../data/raw/reviews_data_raw.csv", skiprows=lambda x: x not in rows_to_keep
-    )
-    print(reviews_data)
+    reviews_data = pd.read_csv("../data/raw/reviews_data_raw.csv")
     reviews_data["emotion_scores_user_reviews"] = reviews_data["user_reviews"].apply(
         lambda x: _get_emotion_scores(classifier, x)
     )
 
-    # print(reviews_data)
+    # TO DEBUG: print(reviews_data)
     # TO-DO: prediction for each user review and aggregate scores
-    # reviews_data.to_csv('reviews_data_with_emotion_scores.csv', index=False)
+    reviews_data = reviews_data[
+        ["mc_link", "title", "num_user_reviews", "emotion_scores_user_reviews"]
+    ]
+    path = "../data/processed/reviews_data_with_emotion_scores.csv"
+    reviews_data.to_csv(path, index=False)
+    print(f":: All done. Saved in {path} ::")
