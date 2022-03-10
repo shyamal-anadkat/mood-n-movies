079fc406ad43ddcce8873f49d616e84a02832d22
shyamalanadkat <sanadkat@grubhub.com>
2022-03-09 19:49:02 -0500

    adds wiki_movie_scraper

--- scripts/imdb_scraper.py
+++ scripts/imdb_scraper.py
@@ -2,149 +2,153 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-page = 1
-movie_df = pd.DataFrame()
+if __name__ == "__main__":
+    page = 1
+    movie_df = pd.DataFrame()
 
-## Credits to Yulin Zheng for some of the logic that we used to adapt the code ##
-while True:
-    url = (
-        "https://www.imdb.com/search/title/?groups=top_1000&languages=en&count=100&start="
-        + str(page)
-        + "&ref_=adv_nxt"
-    )
-    data_page = requests.get(url, headers={"Accept-Language": "en-US"})
-    print(url)
-    titles = []
-    title_hrefs = []
-    mc_links = []
-    certificates = []
-    ratings = []
-    genres = []
-    votes = []
-    metascores = []
-    runtime_list = []
-    directors_list = []
-    stars_list = []
-    grosses_list = []  #
-    introduction_list = []
-    produce_year_list = []
-    soup = BeautifulSoup(data_page.text)
-    list_item_content = soup.findAll("div", class_="lister-item-content")
-    print("number of movies on page: " + str(len(list_item_content)))
-    for movie_content in list_item_content:
-        list_muted_text = movie_content.findAll("p", class_="text-muted")
-        introduction_list.append(list_muted_text[1].text.replace("\n ", ""))
-        item_header = movie_content.find("h3", class_="lister-item-header")
-        title = item_header.find("a")
-        title_href = item_header.find("a", href=True)["href"]
+    ## Credits to Yulin Zheng for some of the logic that we used to adapt the code ##
+    group = "bottom_1000"  # top_1000
+    while True:
+        url = (
+            f"https://www.imdb.com/search/title/?groups={group}&languages=en&count=100&start="
+            + str(page)
+            + "&ref_=adv_nxt"
+        )
+        data_page = requests.get(url, headers={"Accept-Language": "en-US"})
+        print(url)
+        titles = []
+        title_hrefs = []
+        mc_links = []
+        certificates = []
+        ratings = []
+        genres = []
+        votes = []
+        metascores = []
+        runtime_list = []
+        directors_list = []
+        stars_list = []
+        grosses_list = []  #
+        introduction_list = []
+        produce_year_list = []
+        soup = BeautifulSoup(data_page.text)
+        list_item_content = soup.findAll("div", class_="lister-item-content")
+        print("number of movies on page: " + str(len(list_item_content)))
+        for movie_content in list_item_content:
+            list_muted_text = movie_content.findAll("p", class_="text-muted")
+            introduction_list.append(list_muted_text[1].text.replace("\n ", ""))
+            item_header = movie_content.find("h3", class_="lister-item-header")
+            title = item_header.find("a")
+            title_href = item_header.find("a", href=True)["href"]
 
-        # get metacritic link/title
-        critic_url = f"https://www.imdb.com{title_href}criticreviews"
-        mc_page = requests.get(critic_url, headers={"Accept-Language": "en-US"})
-        mc_soup = BeautifulSoup(mc_page.text)
-        mc_link_elem = mc_soup.select_one("a[href*=https://www.metacritic.com/movie/]")
-        if mc_link_elem:
-            mc_link = mc_link_elem["href"]
-        else:
-            mc_link = ""
-            print(f"MC link not available for {title}-{title_href}")
+            # get metacritic link/title
+            critic_url = f"https://www.imdb.com{title_href}criticreviews"
+            mc_page = requests.get(critic_url, headers={"Accept-Language": "en-US"})
+            mc_soup = BeautifulSoup(mc_page.text)
+            mc_link_elem = mc_soup.select_one(
+                'a[href*="https://www.metacritic.com/movie/"]'
+            )
+            if mc_link_elem:
+                mc_link = mc_link_elem["href"]
+            else:
+                mc_link = ""
+                print(f"MC link not available for {title}-{title_href}")
 
-        rating = movie_content.find("strong")
-        certificate = movie_content.find("span", class_="certificate")
-        vote = movie_content.find("p", class_="sort-num_votes-visible")
-        genre = movie_content.find("span", class_="genre")
-        metascore = movie_content.find("span", class_="metascore favorable")
-        runtime = movie_content.find("span", class_="runtime")
-        p_content = movie_content.find("p", class_="")
-        produce_year = movie_content.find("span", class_="lister-item-year")
-        produce_year = produce_year.text.replace("(", "")
-        produce_year = produce_year.replace(")", "")
-        produce_year_list.append(produce_year)
-        if p_content != None:
-            p_content_split = p_content.text.split("|")
-            directors = p_content_split[0].replace("\n", "")
-            directors = directors.replace("Director:", "")
-            directors = directors.replace("Directors:", "")
-            stars = p_content_split[1].replace("\n", "")
-            stars = stars.replace("Stars:", "")
-            stars_list.append(stars)
-            directors_list.append(directors)
-        else:
-            directors_list.append(None)
-            stars_list.append(None)
-        if certificate != None:
-            certificates.append(certificate.text.strip())
-        else:
-            certificates.append(certificate)
-        if "|" in vote.text:
-            vote_gross = vote.text.split("|")
-            vote_value = vote_gross[0]
-            vote_value = vote_value.replace("\n", "")
-            vote_value = vote_value.replace(",", "")
-            vote_value = vote_value.replace("Votes:", "")
-            gross_value = vote_gross[1]
-            gross_value = gross_value.replace("\n", "")
-            gross_value = gross_value.replace("Gross:", "")
-            gross_value = gross_value.replace("$", "")
-            gross_value = gross_value.replace("M", "")
-            gross_value = gross_value.replace(" ", "")
-            votes.append(vote_value)
-            grosses_list.append(gross_value)
-        else:
-            vote_value = vote.text.replace("\n", "")
-            vote_value = vote_value.replace("Votes:", "")
-            vote_value = vote_value.replace(",", "")
-            votes.append(vote_value)
-            grosses_list.append(None)
-        if rating != None:
-            ratings.append(rating.text.strip())
-        else:
-            ratings.append(rating)
-        title_hrefs.append(title_href)
-        if title != None:
-            titles.append(title.text.strip())
-        else:
-            titles.append(title)
-        if mc_link != None:
-            mc_links.append(mc_link)
-        if genre != None:
-            genres_text = genre.text.replace(" ", "")
-            genres_text = genres_text.strip()
-            genres.append(genres_text)
-        else:
-            genres.append(genre)
-        if metascore != None:
-            metascores.append(metascore.text.strip())
-        else:
-            metascores.append(metascore)
-        if runtime != None:
-            runtime_text = runtime.text.strip()
-            runtime_text = runtime_text.replace(" min", "")
-            runtime_list.append(runtime_text)
-        else:
-            runtime_list.append(runtime)
+            rating = movie_content.find("strong")
+            certificate = movie_content.find("span", class_="certificate")
+            vote = movie_content.find("p", class_="sort-num_votes-visible")
+            genre = movie_content.find("span", class_="genre")
+            metascore = movie_content.find("span", class_="metascore favorable")
+            runtime = movie_content.find("span", class_="runtime")
+            p_content = movie_content.find("p", class_="")
+            produce_year = movie_content.find("span", class_="lister-item-year")
+            produce_year = produce_year.text.replace("(", "")
+            produce_year = produce_year.replace(")", "")
+            produce_year_list.append(produce_year)
+            if p_content != None:
+                p_content_split = p_content.text.split("|")
+                directors = p_content_split[0].replace("\n", "")
+                directors = directors.replace("Director:", "")
+                directors = directors.replace("Directors:", "")
+                stars = p_content_split[1].replace("\n", "")
+                stars = stars.replace("Stars:", "")
+                stars_list.append(stars)
+                directors_list.append(directors)
+            else:
+                directors_list.append(None)
+                stars_list.append(None)
+            if certificate != None:
+                certificates.append(certificate.text.strip())
+            else:
+                certificates.append(certificate)
+            if "|" in vote.text:
+                vote_gross = vote.text.split("|")
+                vote_value = vote_gross[0]
+                vote_value = vote_value.replace("\n", "")
+                vote_value = vote_value.replace(",", "")
+                vote_value = vote_value.replace("Votes:", "")
+                gross_value = vote_gross[1]
+                gross_value = gross_value.replace("\n", "")
+                gross_value = gross_value.replace("Gross:", "")
+                gross_value = gross_value.replace("$", "")
+                gross_value = gross_value.replace("M", "")
+                gross_value = gross_value.replace(" ", "")
+                votes.append(vote_value)
+                grosses_list.append(gross_value)
+            else:
+                vote_value = vote.text.replace("\n", "")
+                vote_value = vote_value.replace("Votes:", "")
+                vote_value = vote_value.replace(",", "")
+                votes.append(vote_value)
+                grosses_list.append(None)
+            if rating != None:
+                ratings.append(rating.text.strip())
+            else:
+                ratings.append(rating)
+            title_hrefs.append(title_href)
+            if title != None:
+                titles.append(title.text.strip())
+            else:
+                titles.append(title)
+            if mc_link != None:
+                mc_links.append(mc_link)
+            if genre != None:
+                genres_text = genre.text.replace(" ", "")
+                genres_text = genres_text.strip()
+                genres.append(genres_text)
+            else:
+                genres.append(genre)
+            if metascore != None:
+                metascores.append(metascore.text.strip())
+            else:
+                metascores.append(metascore)
+            if runtime != None:
+                runtime_text = runtime.text.strip()
+                runtime_text = runtime_text.replace(" min", "")
+                runtime_list.append(runtime_text)
+            else:
+                runtime_list.append(runtime)
 
-    df = pd.DataFrame(
-        {
-            "title": titles,
-            "title_href": title_hrefs,
-            "mc_link": mc_links,
-            "genre": genres,
-            "certificate": certificates,
-            "votes": votes,
-            "metascore": metascores,
-            "directors": directors_list,
-            "stars": stars_list,
-            # "gross":grosses_list,
-            "introduction": introduction_list,
-            "produce_year": produce_year_list,
-            "runtime": runtime_list,
-            "rating": ratings,
-        }
-    )
-    movie_df = movie_df.append(df)
-    page = page + 100
-    if page > 1000:
-        break
+        df = pd.DataFrame(
+            {
+                "title": titles,
+                "title_href": title_hrefs,
+                "mc_link": mc_links,
+                "genre": genres,
+                "certificate": certificates,
+                "votes": votes,
+                "metascore": metascores,
+                "directors": directors_list,
+                "stars": stars_list,
+                # "gross":grosses_list,
+                "introduction": introduction_list,
+                "produce_year": produce_year_list,
+                "runtime": runtime_list,
+                "rating": ratings,
+            }
+        )
+        movie_df = movie_df.append(df)
+        page = page + 100
+        if page > 1000:
+            break
 
-movie_df.to_csv("imdb_top_1000.csv", index=False)
+    movie_df.to_csv(f"imdb_{group}.csv", index=False)
---
+++ scripts/wiki_movie_scraper.py
@@ -0,0 +1,132 @@
+# import required modules
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import re
+
+
+def is_right_wikipg(href, imdb_soup):
+    retVal = False
+    href_id = re.findall(r".(tt[0-9]*)", href)[0]
+    imdb_link_elem = imdb_soup.select_one(
+        f'a[href*="https://www.imdb.com/title/{href_id}"]'
+    )
+    if imdb_link_elem:
+        imdb_link, *rest = imdb_link_elem[  # pylint: disable=unused-variable
+            "href"
+        ].split(  # pylint: disable=unused-variable
+            "?"
+        )
+        imdb_link_id = re.findall(r".(tt[0-9]*)", imdb_link)[1]
+        # imdb_link = imdb_link.replace('awards', '').replace('business', '') # make this more generic
+        # to_compare = f"https://www.imdb.com{href}"
+        # print(f"{imdb_link}#######{to_compare}")
+        if imdb_link_id == href_id:
+            retVal = True
+
+    return retVal
+
+
+def sensible_title_caps(title):
+    no_caps_list = ["in", "the"]
+    words = []
+    for word in title.split():
+        if word not in no_caps_list:
+            word = word.capitalize()
+        words.append(word)
+    return " ".join(words)
+
+
+def create_df_with_plots(imdb_df):
+    movie_title = imdb_df[["title", "title_href", "mc_link", "produce_year"]].values
+    num_missing_link = 0
+
+    # example link dune.. https://en.wikipedia.org/wiki/Dune_(2021_film)
+    plots_dicts = list()
+    for title, href, mc_link, year in movie_title[0:31]:
+        # get URL
+        wiki_title = (
+            title.strip()
+            .replace(" ", "_")
+            .replace(".", "")
+            .replace("_In_", "_in_")
+            .strip()
+        )
+        # note - capitalize first letter only if not like a, in
+        wiki_link = f"https://en.wikipedia.org/wiki/{wiki_title}"
+        # print(wiki_link)
+        page = requests.get(wiki_link)
+
+        # scrape webpage
+        soup = BeautifulSoup(page.content, "html.parser")
+        imdb_soup = BeautifulSoup(page.text, "html.parser")
+
+        is_right_wikipage = is_right_wikipg(href, imdb_soup)
+        if not is_right_wikipage:
+            # get the wiki page with like the year and film pattern
+            # I wanna set the soup to that new page if I find it.  West_Side_Story_(2021_film)
+            suffix = f"_({year}_film)"
+            wiki_link = f"https://en.wikipedia.org/wiki/{wiki_title}{suffix}"
+            page = requests.get(wiki_link)
+            soup = BeautifulSoup(page.content, "html.parser")
+            imdb_soup = BeautifulSoup(page.text, "html.parser")
+
+            is_right_wikipage = is_right_wikipg(href, imdb_soup)
+            if not is_right_wikipage:
+                suffix = "_(film)"
+                wiki_link = f"https://en.wikipedia.org/wiki/{wiki_title}{suffix}"
+                page = requests.get(wiki_link)
+                soup = BeautifulSoup(page.content, "html.parser")
+                imdb_soup = BeautifulSoup(page.text, "html.parser")
+                mc_link = list()
+                mc_link.append(
+                    imdb_soup.select_one('a[href*="https://www.imdb.com/title/"]')
+                )
+                is_right_wikipage = is_right_wikipg(href, imdb_soup)
+                if not is_right_wikipage:
+                    num_missing_link += 1
+                    missing_movies = list()
+                    missing_movies.append(wiki_title)
+                    print(f"IMDB link not available for {wiki_title}, link:{wiki_link}")
+
+        if is_right_wikipage:
+            # create title object
+            title = soup.find(id="firstHeading").text
+            title_movie = list()
+            title_movie.append(title)
+
+            plots = []
+            plot = []
+            plot_section = [soup.findAll("h2")[1]]
+            for header in plot_section:
+                for elem in header.next_siblings:
+                    if elem.name and elem.name.startswith("h2"):
+                        # stop at next header
+                        break
+                    if elem.name == "p":
+                        plot.append(elem.get_text().replace("\n", ""))
+
+            plots.append(plot)
+            plots_dic = {
+                "title": title_movie,
+                "wiki_link": wiki_link,
+                "plot": plots,
+                "mc_link": mc_link,
+            }
+            print(f"{title}-{year}")
+            plots_dicts.append(plots_dic)
+    print("======================================")
+    print(f"# of Missing links = {num_missing_link}")
+    print("======================================")
+    return pd.DataFrame(plots_dicts, columns=["title", "wiki_link", "plot", "mc_link"])
+
+
+if __name__ == "__main__":
+    in_path = "../data/processed/imdb_top_1000_clean.csv"
+    out_path = "../data/processed/with_plot_summary.csv"
+    print(
+        f":: Scraping plots for wikipedia using input CSV: {in_path}, please wait...::"
+    )
+    df = pd.read_csv(in_path)
+    create_df_with_plots(df).to_csv(out_path, index=False)
+    print(f":: All done. Saved in {out_path} ::")
